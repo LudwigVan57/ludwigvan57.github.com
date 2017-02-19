52187c1b81cccb1ac18b07387f0b17f73a7008ec
beethovenludwig <zyfan2011@163.com>
2017-02-19 16:12:42 +0800

    3d canvas

--- scripts/projects/3dCanvas.js
+++ scripts/projects/3dCanvas.js
@@ -3,38 +3,50 @@
 
     canvas.attr({width:$(window).width(),height:($('.fixed-bottom').height()-$('.desc').height())});
 
-
-    var size = 50;
-
     var canvas3d = new Canvas3d(canvas[0]);
 
+    var meshSize = 50,size = 2,cubeNum = 2;
+
     function startDraw(){
-        var num = 1;
-        for(var i=-num;i<=num;i=i+1){
-            for(var j=-num;j<=num;j=j+1){
+        var array = [];
+
+        for(var i=-size+1;i<size;i++){
+            for(var j=-size+1;j<size;j++){
                 addPark(i,j);
+                array.push([i,j]);
             }
         }
 
-        addCube(0,0);
+        for(var c=0;c<cubeNum;c++){
+            addCube.apply(this, getOneQuad(array)[0]);
+        }
+
         canvas3d.draw();
     }
 
-    function addCube(x,y){
-        var centerX = 2*x*size,
-            centerY = 2*y*size;
+    function getOneQuad(array){
+        return array.splice(getRandomBetween(0,array.length-1),1);
+    }
 
-        canvas3d.addCube(new Point(centerX-size,centerY+size,-1),new Point(centerX+size,centerY+size,-1),
-            new Point(centerX+size,centerY-size,-1),new Point(centerX-size,centerY-size,-1),
-        new Point(centerX,centerX,-size-1));
+    function getRandomBetween(min,max){
+        return Math.floor(Math.random()*(max-min+1)+min);
+    }
+
+    function addCube(x,y){
+        var centerX = 2*x*meshSize,
+            centerY = 2*y*meshSize;
+
+        canvas3d.addCube(new Point(centerX-meshSize,centerY+meshSize,-1),new Point(centerX+meshSize,centerY+meshSize,-1),
+            new Point(centerX+meshSize,centerY-meshSize,-1),new Point(centerX-meshSize,centerY-meshSize,-1),
+        new Point(centerX,centerY,-meshSize-1));
     }
 
     function addPark(x,y){
-        var centerX = 2*x*size,
-            centerY = 2*y*size;
+        var centerX = 2*x*meshSize,
+            centerY = 2*y*meshSize;
 
-        canvas3d.addQuad(new Point(centerX-size,centerY+size,0),new Point(centerX+size,centerY+size,0),
-            new Point(centerX+size,centerY-size,0),new Point(centerX-size,centerY-size,0));
+        canvas3d.addQuad(new Point(centerX-meshSize,centerY+meshSize,0),new Point(centerX+meshSize,centerY+meshSize,0),
+            new Point(centerX+meshSize,centerY-meshSize,0),new Point(centerX-meshSize,centerY-meshSize,0));
     }
 
 
